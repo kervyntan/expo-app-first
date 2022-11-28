ea2d7cf5aeaf427485ea00140cd7dae767376551
iamkervyntan <kervyntan@gmail.com>
2022-11-29 00:44:37 +0800

    added Image component

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native';
 
 export default function App() {
+
   return (
     // SafeAreaView -> adds padding to prevent content from being blocked by notches
     <SafeAreaView style={styles.container}>
-      <Text>But don't vibe too much. </Text>
+      <Text> This is a test </Text>
+      <Image style={styles.logo} source={{ uri : "https://picsum.photos/200" }} />
       <StatusBar style="auto" />
     </SafeAreaView>
   );
@@ -19,4 +21,8 @@ const styles = StyleSheet.create({
     // alignItems: 'center',
     // justifyContent: 'center'
   },
+  logo : {
+    width : 200,
+    height : 200
+  }
 });
